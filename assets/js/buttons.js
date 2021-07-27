0967e3409ac5ff21a01eb46eae008667de35f8dd
ElrieM <elrie.mouton@gmail.com>
2021-07-27 21:18:28 +0000

    fix: resolve issue with generating unique grid numbers across rows and columns

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -1,33 +1,10 @@
-// New puzzle generates on click of button
-// Cells can only have a value of 1 to 9
-// Each box can only contain a number once
-// Some cells set to hide value, value compared to user input
-
-window.onload = createPuzzle();
-
-function createPuzzle() {
-    for (let i = 1; i < 10; i++) { // Runs through each of the 9 boxes
-        let box = document.getElementById(`box-${i}`);
-        box.innerHTML = ""; // Clears previous grid
-
-        var testBox = [];
-
-        for (let j = 1; j < 10; j++) { // Runs through each of the 9 cells within a box
-            let value = Math.floor(Math.random() * 9 + 1); // Assigns a value between 1 and 9
-            console.log(value);
-
-            while (testBox.includes(value)) {
-                if (j === 1) {
-                    testBox.push(value);
-                } else {
-                    value = Math.floor(Math.random() * 9 + 1); // Assigns a value between 1 and 9
-                    console.log(`New value:${value}`);
-                }
-            }
-
-            testBox.push(value);
-
-            box.innerHTML += `<div class="cell">${value}</div>`;
-        }
+// Runs practice game
+// Practice game has the ability to play various game
+function runPractice() {
+    window.location.href = 'game.html';
     }
-}
+
+// Runs challenge game
+    function runChallenge() {
+        window.location.href = 'game.html';
+    }
