ea377175e155f2b4e93c921619048eb69db19b27
Elrie <79701587+ElrieM@users.noreply.github.com>
2021-08-08 19:17:43 +0200

    fix: resolve issue where display solution showed play grid values

--- assets/js/practice.js
+++ assets/js/practice.js
@@ -7,7 +7,7 @@ function practiceGame() {
     document.getElementById("newButton").addEventListener("click", resetTimer);
 
     // Solve game button: shows puzzle solution and resets stops stopwatch timer
-    document.getElementById("solveButton").addEventListener("click", solveGrid);
+    document.getElementById("solveButton").addEventListener("click", displaySolvedGrid);
     document.getElementById("solveButton").addEventListener("click", stopTimer);
 
     // Restart game button: creates new puzzle and resets stopwatch timer
@@ -34,10 +34,20 @@ function practiceGame() {
         [1, 8, 9, 3, 4, 7, 2, 6, 5]
     ];
 
-    var playGrid = [];
+    var playGrid = [
+        [8, 6, 1, 7, 9, 4, 3, 5, 2],
+        [3, 5, 2, 1, 6, 8, 7, 4, 9],
+        [4, 9, 7, 2, 5, 3, 1, 8, 6],
+        [2, 1, 8, 9, 7, 5, 6, 3, 4],
+        [6, 7, 5, 3, 4, 1, 9, 2, 8],
+        [9, 3, 4, 6, 8, 2, 5, 1, 7],
+        [5, 2, 6, 8, 1, 9, 4, 7, 3],
+        [7, 4, 3, 5, 2, 6, 8, 9, 1],
+        [1, 8, 9, 3, 4, 7, 2, 6, 5]
+    ];
 
     // Create game grid solution
-    function solveGrid(solvedGrid) {
+    function solveGrid(solvedGrid, playGrid) {
 
         let randomTimes = Math.floor((Math.random() * 8) + 1); // Randomise grid by adding 1 for a random amount of times
 
@@ -46,20 +56,22 @@ function practiceGame() {
                 solvedGrid[i][j] += randomTimes;
                 solvedGrid[i][j] %= 9;
                 solvedGrid[i][j]++;
+                playGrid[i][j] = solvedGrid[i][j];
             }
         }
 
         console.log(randomTimes);
         console.log(solvedGrid);
+        console.log(playGrid);
 
-        return solvedGrid;
+        return solvedGrid, playGrid;
     }
 
-    solveGrid(solvedGrid);
+    solveGrid(solvedGrid, playGrid);
 
     // Create play grid, empty randomly selected cells
     function createPracticeGame(playGrid) {
-        playGrid = solvedGrid;
+        gridSolution = playGrid;
 
         let totalBlank = 24; // Easy / Medium / Hard
         let randomCell;
@@ -74,11 +86,12 @@ function practiceGame() {
         }
 
         console.log(playGrid);
+        console.log(solvedGrid);
         return playGrid;
     }
 
 
-
+    // Display game grid, showing nil values as blank cells
     function displayPracticeGrid() {
 
         displayGrid = createPracticeGame(playGrid);
@@ -125,6 +138,59 @@ function practiceGame() {
             }
         }
     }
+
+    // Display solved grid
+    function displaySolvedGrid() {
+
+        displayGridSolution = solvedGrid;
+
+        console.log(displayGridSolution);
+
+        for (i = 1; i < 10; i++) { // Box number
+            let solBox = document.getElementById(`box-${i}`);
+            solBox.innerHTML = ""; // Clears previous grid
+
+            // Sets value of row based on box
+            if (i === 1 || i === 2 || i === 3) {
+                rowLowerSol = 1 - 1;
+                rowUpperSol = 3;
+            } else if (i === 4 || i === 5 || i === 6) {
+                rowLowerSol = 4 - 1;
+                rowUpperSol = 6;
+            } else if (i === 7 || i === 8 || i === 9) {
+                rowLowerSol = 7 - 1;
+                rowUpperSol = 9;
+            }
+
+            // Sets value of column based on box
+            if (i === 1 || i === 4 || i === 7) {
+                colLowerSol = 1 - 1;
+                colUpperSol = 3;
+            } else if (i === 2 || i === 5 || i === 8) {
+                colLowerSol = 4 - 1;
+                colUpperSol = 6;
+            } else if (i === 3 || i === 6 || i === 9) {
+                colLowerSol = 7 - 1;
+                colUpperSol = 9;
+            }
+
+            // Displays values in grid
+            for (let j = rowLowerSol; j < rowUpperSol; j++) { // Row index
+                for (let k = colLowerSol; k < colUpperSol; k++) { // Column index
+                    if (displayGridSolution[j][k] === 0) {
+                        solBox.innerHTML += `<div class="cell"></div>`;
+                    } else {
+                        solBox.innerHTML += `<div class="cell">${(displayGridSolution[j][k])}</div>`;
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
+
 
 }
 
