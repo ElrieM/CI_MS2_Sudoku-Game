bd3f0480e310be6d9b23e14ecb9fdf43fb4c32c1
Elrie <79701587+ElrieM@users.noreply.github.com>
2021-08-16 00:28:04 +0200

    feat: Add modal for timer pause button (practice game)

--- assets/js/practice.js
+++ assets/js/practice.js
@@ -2,23 +2,93 @@
 
 window.onload = function () {
     practiceGame();
+    displayPracticeGrid();
     startTimer();
 };
 
-var levelBlank = 20;
+var modal = document.getElementById('pauseModal');
+var close = document.getElementById('close-btn');
+var closeX = document.getElementById('close-top');
 
+// Game control buttons
+// New game button: creates new puzzle and resets stopwatch timer
+document.getElementById("newButton").addEventListener("click", function () {
+    if (totalBlank == 0) { // Resets blank cells (changes to nil if game grid is reset)
+        totalBlank = levelBlank;
+    }
+    practiceGame(); // Generates new puzzle
+    displayPracticeGrid(); // Displays puzzle with blank cells for game
+    resetTimer(); // Resets timer to start from nil
+});
+
+// Solve game button: shows puzzle solution and resets stops stopwatch timer
+// Alert adapted from https://www.tutorialspoint.com/How-to-delay-a-JavaScript-function-call-using-JavaScript
+document.getElementById("solveButton").addEventListener("click", function () {
+    displaySolvedGrid(); // Displays solved game grid
+    setTimeout(function () {
+        alert("Play again?");
+        practiceGame(); // Generates new game
+        displayPracticeGrid(); // Displays puzzle with blank cells for new game
+        resetTimer(); // Resets timer to start from nil
+    }, 100);
+});
+
+// Restart game button: creates new puzzle and resets stopwatch timer
+document.getElementById("restartButton").addEventListener("click", function () {
+    totalBlank = 0;
+    displayPracticeGrid();
+    startTimer(); // Continues countdown (in case game is paused when restarted)
+});
+
+// Timer control buttons
+// Button to start timer (continue after pausing)
+document.getElementById("start").addEventListener("click", startTimer);
+
+// Button to stop timer (pauses game)
+document.getElementById("stop").addEventListener("click", function () {
+    stopTimer();
+    modal.style.display = "block";
+});
+
+// Puzzle creating function Adapted from https://github.com/reymon359/web-experiments/blob/master/Sudoku%20Board%20Generator/script.js
+
+// Initial solved grid
+var solvedGrid = [
+    [8, 6, 1, 7, 9, 4, 3, 5, 2],
+    [3, 5, 2, 1, 6, 8, 7, 4, 9],
+    [4, 9, 7, 2, 5, 3, 1, 8, 6],
+    [2, 1, 8, 9, 7, 5, 6, 3, 4],
+    [6, 7, 5, 3, 4, 1, 9, 2, 8],
+    [9, 3, 4, 6, 8, 2, 5, 1, 7],
+    [5, 2, 6, 8, 1, 9, 4, 7, 3],
+    [7, 4, 3, 5, 2, 6, 8, 9, 1],
+    [1, 8, 9, 3, 4, 7, 2, 6, 5]
+];
+
+// Initial play grid (same as solved grid until blank cells are removed)
+// Sets array form of 9 rows with 9 values each
+var playGrid = [
+    [8, 6, 1, 7, 9, 4, 3, 5, 2],
+    [3, 5, 2, 1, 6, 8, 7, 4, 9],
+    [4, 9, 7, 2, 5, 3, 1, 8, 6],
+    [2, 1, 8, 9, 7, 5, 6, 3, 4],
+    [6, 7, 5, 3, 4, 1, 9, 2, 8],
+    [9, 3, 4, 6, 8, 2, 5, 1, 7],
+    [5, 2, 6, 8, 1, 9, 4, 7, 3],
+    [7, 4, 3, 5, 2, 6, 8, 9, 1],
+    [1, 8, 9, 3, 4, 7, 2, 6, 5]
+];
+
+var levelBlank = 20; // Initial game level set to easy
 var level = document.getElementById('level-select');
+
 // Assign number of cells blank based on level selection
 // Adapted from https://stackoverflow.com/questions/37538217/how-to-get-addeventlistener-to-work-with-a-select-tag
+
 level.addEventListener("change", function () {
     levelBlank = this.value;
     practiceGame();
-}, false);
-
-// Generates game for play, starting with solution and then creating grid for play with random blank cells
-function practiceGame() {
-
-    totalBlank = levelBlank; // fetches number of blank cells to generate
+    displayPracticeGrid();
 
     if (totalBlank == undefined) {
         totalBlank = 20;
@@ -26,71 +96,13 @@ function practiceGame() {
 
     localStorage.setItem('totalBlank', levelBlank); // Stores number of cells to be blank for later usage in play grid
 
-    // New game button: creates new puzzle and resets stopwatch timer
-    document.getElementById("newButton").addEventListener("click", function () {
-        if (totalBlank == 0) {
-            totalBlank = levelBlank;
-        }
-        solveGrid(solvedGrid, playGrid); // generates solution
-        createPracticeGame(playGrid); // generates game grid
-        displayPracticeGrid(); // display game grid
-        resetTimer(); // resets timer
-    });
+}, false);
 
-    // Solve game button: shows puzzle solution and resets stops stopwatch timer
-    // Alert adapted from https://www.tutorialspoint.com/How-to-delay-a-JavaScript-function-call-using-JavaScript
-    document.getElementById("solveButton").addEventListener("click", function () {
-        displaySolvedGrid(); // Displays solved game grid
-        setTimeout(function () {
-            alert("Play again?");
-            solveGrid(solvedGrid, playGrid);
-            createPracticeGame(playGrid);
-            displayPracticeGrid();
-            resetTimer();
-        }, 100);
-    });
+// Generates game for play, starting with solution and then creating grid for play with random blank cells
+function practiceGame() {
 
-    // Restart game button: creates new puzzle and resets stopwatch timer
-    document.getElementById("restartButton").addEventListener("click", function () {
-        totalBlank = 0;
-        displayPracticeGrid();
-        startTimer(); // Continues countdown (in case game is paused when restarted)
-    });
-
-    // Button to start timer
-    document.getElementById("start").addEventListener("click", startTimer);
-
-    // Button to stop timer
-    document.getElementById("stop").addEventListener("click", stopTimer);
-
-    // Puzzle creating function Adapted from https://github.com/reymon359/web-experiments/blob/master/Sudoku%20Board%20Generator/script.js
-
-    // Initial solved grid
-    var solvedGrid = [
-        [8, 6, 1, 7, 9, 4, 3, 5, 2],
-        [3, 5, 2, 1, 6, 8, 7, 4, 9],
-        [4, 9, 7, 2, 5, 3, 1, 8, 6],
-        [2, 1, 8, 9, 7, 5, 6, 3, 4],
-        [6, 7, 5, 3, 4, 1, 9, 2, 8],
-        [9, 3, 4, 6, 8, 2, 5, 1, 7],
-        [5, 2, 6, 8, 1, 9, 4, 7, 3],
-        [7, 4, 3, 5, 2, 6, 8, 9, 1],
-        [1, 8, 9, 3, 4, 7, 2, 6, 5]
-    ];
-
-    // Initial play grid (same as solved grid until blank cells are removed)
-    // Sets array form of 9 rows with 9 values each
-    var playGrid = [
-        [8, 6, 1, 7, 9, 4, 3, 5, 2],
-        [3, 5, 2, 1, 6, 8, 7, 4, 9],
-        [4, 9, 7, 2, 5, 3, 1, 8, 6],
-        [2, 1, 8, 9, 7, 5, 6, 3, 4],
-        [6, 7, 5, 3, 4, 1, 9, 2, 8],
-        [9, 3, 4, 6, 8, 2, 5, 1, 7],
-        [5, 2, 6, 8, 1, 9, 4, 7, 3],
-        [7, 4, 3, 5, 2, 6, 8, 9, 1],
-        [1, 8, 9, 3, 4, 7, 2, 6, 5]
-    ];
+    totalBlank = levelBlank; // fetches number of blank cells to generate
+    console.log(totalBlank);
 
     // Create game grid solution
     function solveGrid(solvedGrid, playGrid) {
@@ -130,109 +142,106 @@ function practiceGame() {
         }
         return playGrid;
     }
+    createPracticeGame(playGrid);
+    return solvedGrid, playGrid;
+}
 
-    // Display game grid, showing nil values as blank cells
-    function displayPracticeGrid() {
+// Display game grid, showing nil values as blank cells
+function displayPracticeGrid() {
 
-        displayGrid = createPracticeGame(playGrid);
+    displayGrid = playGrid;
 
-        for (i = 1; i < 10; i++) { // Box number
-            let box = document.getElementById(`box-${i}`);
-            box.innerHTML = ""; // Clears previous grid
+    for (i = 1; i < 10; i++) { // Box number
+        let box = document.getElementById(`box-${i}`);
+        box.innerHTML = ""; // Clears previous grid
 
-            // Sets value of row based on box
-            if (i === 1 || i === 2 || i === 3) {
-                rowLower = 1 - 1;
-                rowUpper = 3;
-            } else if (i === 4 || i === 5 || i === 6) {
-                rowLower = 4 - 1;
-                rowUpper = 6;
-            } else if (i === 7 || i === 8 || i === 9) {
-                rowLower = 7 - 1;
-                rowUpper = 9;
-            }
+        // Sets value of row based on box
+        if (i === 1 || i === 2 || i === 3) {
+            rowLower = 1 - 1;
+            rowUpper = 3;
+        } else if (i === 4 || i === 5 || i === 6) {
+            rowLower = 4 - 1;
+            rowUpper = 6;
+        } else if (i === 7 || i === 8 || i === 9) {
+            rowLower = 7 - 1;
+            rowUpper = 9;
+        }
 
-            // Sets value of column based on box
-            if (i === 1 || i === 4 || i === 7) {
-                colLower = 1 - 1;
-                colUpper = 3;
-            } else if (i === 2 || i === 5 || i === 8) {
-                colLower = 4 - 1;
-                colUpper = 6;
-            } else if (i === 3 || i === 6 || i === 9) {
-                colLower = 7 - 1;
-                colUpper = 9;
-            }
+        // Sets value of column based on box
+        if (i === 1 || i === 4 || i === 7) {
+            colLower = 1 - 1;
+            colUpper = 3;
+        } else if (i === 2 || i === 5 || i === 8) {
+            colLower = 4 - 1;
+            colUpper = 6;
+        } else if (i === 3 || i === 6 || i === 9) {
+            colLower = 7 - 1;
+            colUpper = 9;
+        }
 
-            // Displays values in grid
-            for (let j = rowLower; j < rowUpper; j++) { // Row index
-                for (let k = colLower; k < colUpper; k++) { // Column index
-                    if (displayGrid[j][k] === 0) {
-                        box.innerHTML += `
+        // Displays values in grid
+        for (let j = rowLower; j < rowUpper; j++) { // Row index
+            for (let k = colLower; k < colUpper; k++) { // Column index
+                if (displayGrid[j][k] === 0) {
+                    box.innerHTML += `
                         <div class="cell">
                         <label for inputVal></label>
                         <input class="input-cell" name="inputVal" id="inputVal" type="number" step="1" min="1" max="9" maxlength="1"></div>`;
-                    } else {
-                        box.innerHTML += `<div class="cell">${(displayGrid[j][k])}</div>`;
-                    }
+                } else {
+                    box.innerHTML += `<div class="cell">${(displayGrid[j][k])}</div>`;
                 }
             }
         }
     }
-
-    // Display practice grid on load
-    displayPracticeGrid();
-
-    // Display solved grid
-    function displaySolvedGrid() {
-
-        displayGridSolution = solvedGrid;
-
-        for (i = 1; i < 10; i++) { // Box number
-            let solBox = document.getElementById(`box-${i}`);
-            solBox.innerHTML = ""; // Clears previous grid
-
-            // Sets value of row based on box
-            if (i === 1 || i === 2 || i === 3) {
-                rowLowerSol = 1 - 1;
-                rowUpperSol = 3;
-            } else if (i === 4 || i === 5 || i === 6) {
-                rowLowerSol = 4 - 1;
-                rowUpperSol = 6;
-            } else if (i === 7 || i === 8 || i === 9) {
-                rowLowerSol = 7 - 1;
-                rowUpperSol = 9;
-            }
-
-            // Sets value of column based on box
-            if (i === 1 || i === 4 || i === 7) {
-                colLowerSol = 1 - 1;
-                colUpperSol = 3;
-            } else if (i === 2 || i === 5 || i === 8) {
-                colLowerSol = 4 - 1;
-                colUpperSol = 6;
-            } else if (i === 3 || i === 6 || i === 9) {
-                colLowerSol = 7 - 1;
-                colUpperSol = 9;
-            }
-
-            // Displays values in grid
-            for (let j = rowLowerSol; j < rowUpperSol; j++) { // Row index
-                for (let k = colLowerSol; k < colUpperSol; k++) { // Column index
-                    if (displayGridSolution[j][k] === 0) {
-                        solBox.innerHTML += `<div class="cell"></div>`;
-                    } else {
-                        solBox.innerHTML += `<div class="cell">${(displayGridSolution[j][k])}</div>`;
-                    }
-                }
-            }
-        }
-    }
-
-    console.log(solvedGrid);
-
 }
 
+// Display solved grid
+function displaySolvedGrid() {
+
+    displayGridSolution = solvedGrid;
+
+    for (i = 1; i < 10; i++) { // Box number
+        let solBox = document.getElementById(`box-${i}`);
+        solBox.innerHTML = ""; // Clears previous grid
+
+        // Sets value of row based on box
+        if (i === 1 || i === 2 || i === 3) {
+            rowLowerSol = 1 - 1;
+            rowUpperSol = 3;
+        } else if (i === 4 || i === 5 || i === 6) {
+            rowLowerSol = 4 - 1;
+            rowUpperSol = 6;
+        } else if (i === 7 || i === 8 || i === 9) {
+            rowLowerSol = 7 - 1;
+            rowUpperSol = 9;
+        }
+
+        // Sets value of column based on box
+        if (i === 1 || i === 4 || i === 7) {
+            colLowerSol = 1 - 1;
+            colUpperSol = 3;
+        } else if (i === 2 || i === 5 || i === 8) {
+            colLowerSol = 4 - 1;
+            colUpperSol = 6;
+        } else if (i === 3 || i === 6 || i === 9) {
+            colLowerSol = 7 - 1;
+            colUpperSol = 9;
+        }
+
+        // Displays values in grid
+        for (let j = rowLowerSol; j < rowUpperSol; j++) { // Row index
+            for (let k = colLowerSol; k < colUpperSol; k++) { // Column index
+                if (displayGridSolution[j][k] === 0) {
+                    solBox.innerHTML += `<div class="cell"></div>`;
+                } else {
+                    solBox.innerHTML += `<div class="cell">${(displayGridSolution[j][k])}</div>`;
+                }
+            }
+        }
+    }
+}
+
+console.log(solvedGrid);
 
 // Countup timer on loading page
 // Adapted from https://dev.to/gspteck/create-a-stopwatch-in-javascript-2mak
@@ -289,3 +298,21 @@ function resetTimer() {
     sec = 0;
     startTimer();
 }
+
+// Modal for pause button, adapted from https://www.w3schools.com/howto/howto_css_modals.asp
+// When the user clicks on close button, close the modal
+close.onclick = function () {
+    modal.style.display = "none";
+};
+
+// When the user clicks on the close X at the top, close the modal
+closeX.onclick = function () {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close the modal
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
