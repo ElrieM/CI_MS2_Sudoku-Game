8115847cf9a6521171a34e5d76e449bdc488ad36
Elrie <79701587+ElrieM@users.noreply.github.com>
2021-08-20 21:12:02 +0200

    fix: repair contact form sending issue

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,8 +1,8 @@
+/* jshint esversion: 6 */
+
 window.onload = function () {
     document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
         emailjs.sendForm('Sudoku', 'MS2-Contact-Form', this)
             .then(function () {
