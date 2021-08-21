ca8c62882c61011483b1cb7241fbfd44a37e5c1e
Elrie <79701587+ElrieM@users.noreply.github.com>
2021-08-21 17:06:48 +0200

    docs: apply jsHint changes to contact page

---
+++ assets/js/contact.js
@@ -0,0 +1,6 @@
+/* jshint esversion: 6 */
+
+// Submit button
+document.getElementById("submit-btn").addEventListener("click", function () {
+    document.getElementById('confirm-submit').innerHTML = `<p>Message sent</p>`;
+});
--- assets/js/email.js
+++ assets/js/email.js
@@ -1,5 +1,7 @@
 /* jshint esversion: 6 */
 
+// Adapted from https://www.emailjs.com/docs/sdk/send-form/
+
 window.onload = function () {
     document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
